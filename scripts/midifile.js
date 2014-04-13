58d0f11af2df0e733db7c26d0329e55e41f32d9a
mobyvb <mobyvb@gmail.com>
2014-04-13 12:07:19 -0400

    add piano to screen

---
+++ scripts/midifile.js
@@ -0,0 +1,62 @@
+var midiUpload = new Dropzone('#midi-upload', {url:'/', autoProcessQueue:false});
+var song = {};
+
+midiUpload.on('addedfile', function(file) {
+  uploadMidiFile(file);
+});
+
+function uploadMidiFile(file) {
+  if (file) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var buffer = e.target.result;
+      song = midiConverter.midiToJson(buffer);
+      updateDownloadLink();
+    };
+    reader.readAsBinaryString(file);
+  }
+}
+
+function updateDownloadLink() {
+  var midi = midiConverter.jsonToMidi(song);
+  var blob = new Blob([stringToArrayBuffer(midi)], {type:'audio/midi'});
+  url = window.URL.createObjectURL(blob);
+  $('#midi-download').attr('href', url);
+}
+
+function stringToArrayBuffer(string) {
+  return stringToUint8Array(string).buffer;
+}
+function stringToBinary(string) {
+  var chars, code, i, isUCS2, len, _i;
+
+  len = string.length;
+  chars = [];
+  isUCS2 = false;
+  for (i = _i = 0; 0 <= len ? _i < len : _i > len; i = 0 <= len ? ++_i : --_i) {
+      code = String.prototype.charCodeAt.call(string, i);
+      if (code > 255) {
+          isUCS2 = true;
+          chars = null;
+          break;
+      } else {
+          chars.push(code);
+      }
+  }
+  if (isUCS2 === true) {
+      return unescape(encodeURIComponent(string));
+  } else {
+      return String.fromCharCode.apply(null, Array.prototype.slice.apply(chars));
+  }
+}
+function stringToUint8Array(string) {
+  var binary, binLen, buffer, chars, i, _i;
+  binary = stringToBinary(string);
+  binLen = binary.length;
+  buffer = new ArrayBuffer(binLen);
+  chars  = new Uint8Array(buffer);
+  for (i = _i = 0; 0 <= binLen ? _i < binLen : _i > binLen; i = 0 <= binLen ? ++_i : --_i) {
+      chars[i] = String.prototype.charCodeAt.call(binary, i);
+  }
+  return chars;
+}
--- scripts/sequencer.js
+++ scripts/sequencer.js
@@ -1,63 +1,13 @@
-var midiUpload = new Dropzone('#midi-upload', {url:'/', autoProcessQueue:false});
-var jsonFile = {};
-
-midiUpload.on('addedfile', function(file) {
-  uploadMidiFile(file);
-});
-
-function uploadMidiFile(file) {
-  if (file) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      var buffer = e.target.result;
-      var toJson = midiConverter.midiToJson(buffer);
-      jsonFile = toJson;
-      updateDownloadLink();
-    };
-    reader.readAsBinaryString(file);
-  }
+var keys = $('.keys');
+for(var pitch=108; pitch>=21; pitch--) { // A0 to C8
+  var note = noteFromMidiPitch(pitch);
+  var noteClass = note.indexOf('#')>-1 ? 'sharp':'';
+  keys.append('<li class="'+noteClass+'">'+note+'</li>')
 }
 
-function updateDownloadLink() {
-  var midi = midiConverter.jsonToMidi(jsonFile);
-  var blob = new Blob([stringToArrayBuffer(midi)], {type:'audio/midi'});
-  url = window.URL.createObjectURL(blob);
-  $('#midi-download').attr('href', url);
-}
-
-function stringToArrayBuffer(string) {
-  return stringToUint8Array(string).buffer;
-}
-function stringToBinary(string) {
-    var chars, code, i, isUCS2, len, _i;
-
-    len = string.length;
-    chars = [];
-    isUCS2 = false;
-    for (i = _i = 0; 0 <= len ? _i < len : _i > len; i = 0 <= len ? ++_i : --_i) {
-        code = String.prototype.charCodeAt.call(string, i);
-        if (code > 255) {
-            isUCS2 = true;
-            chars = null;
-            break;
-        } else {
-            chars.push(code);
-        }
-    }
-    if (isUCS2 === true) {
-        return unescape(encodeURIComponent(string));
-    } else {
-        return String.fromCharCode.apply(null, Array.prototype.slice.apply(chars));
-    }
-}
-function stringToUint8Array(string) {
-    var binary, binLen, buffer, chars, i, _i;
-    binary = stringToBinary(string);
-    binLen = binary.length;
-    buffer = new ArrayBuffer(binLen);
-    chars  = new Uint8Array(buffer);
-    for (i = _i = 0; 0 <= binLen ? _i < binLen : _i > binLen; i = 0 <= binLen ? ++_i : --_i) {
-        chars[i] = String.prototype.charCodeAt.call(binary, i);
-    }
-    return chars;
+function noteFromMidiPitch(p) {
+  var noteDict = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+  var octave = Math.floor((p-12)/12);
+  var note = noteDict[p-octave*12-12];
+  return note+octave;
 }
